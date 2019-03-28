b0bc9f314f97eecfb6e57aa125cb6e7d8334b960
Ivan <4esoquevedoi@gmail.com>
2019-03-28 11:26:14 +0100

    edit profile logic finished

--- routes/profile.js
+++ routes/profile.js
@@ -6,16 +6,18 @@ const User = require('../models/user');
 
 const { isLoggedIn, isNotLoggedIn, validationLoggin } = require('../helpers/middlewares');
 
+// router.post('/edit', isLoggedIn(), (req, res, next) => {
+//   console.log(req.body);
+//   res.status(200).json({ message: req.body });
+// });
+
 router.post('/edit', isLoggedIn(), async (req, res, next) => {
-  console.log('HOOOOOOOOLzzzzzzzzzzzzzzzzzzzzzzzzsdfewsdA');
-  const { username, password, quote, interests, currentUser, currentPassword } = req.body;
+  const { username, password, quote, interests, currentPassword } = req.body;
 
-  let filteredNoChanges = [username, quote, interests].filter(e => typeof e !== 'undefined');
-
-  console.log(filteredNoChanges);
+  const { currentUser } = req.session;
 
   try {
-    if (username !== currentUser) {
+    if (username !== currentUser.username) {
       const userExists = await User.findOne({ username });
 
       if (userExists) {
@@ -26,19 +28,17 @@ router.post('/edit', isLoggedIn(), async (req, res, next) => {
       }
     }
 
-    const verifyUser = await User.findOne({ username });
-
-    if (bcrypt.compareSync(currentPassword, verifyUser.password)) {
+    if (bcrypt.compareSync(currentPassword, currentUser.password)) {
       let editedUser;
       if (password) {
+        console.log(password, 'HAY CONTASEÑAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
         const salt = bcrypt.genSaltSync(10);
         const hashPass = bcrypt.hashSync(password, salt);
-        editedUser = await User.findOneAndUpdate({ currentUser }, { $set: { username, password: hashPass, quote, interests } });
-      } else {
-        editedUser = await User.findOneAndUpdate({ currentUser }, { $set: { ...filteredNoChanges } });
-      }
 
-      console.log('HOOOOOOOOLzzzzzzzzzzzzzzzzzzzzzzzzsdfewsdA');
+        editedUser = await User.findOneAndUpdate({ username: currentUser.username }, { $set: { username, password: hashPass, quote, interests } }, { new: true });
+      } else {
+        editedUser = await User.findOneAndUpdate({ username: currentUser.username }, { $set: { username, quote, interests } }, { new: true });
+      }
 
       req.session.currentUser = editedUser;
 
