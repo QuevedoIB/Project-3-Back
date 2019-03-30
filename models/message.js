3d3c92dd9177e4f0b1bcac3da907d9ba22acbda4
Ester Fernandez <esterfernand95@gmail.com>
2019-03-30 15:56:08 +0100

    created message model

---
+++ models/message.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const messageSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  date: {
+    type: Date,
+  }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
