037bd1c679f37b366db6019df72487902920f98d
Ester Fernandez <esterfernand95@gmail.com>
2019-03-30 14:51:35 +0100

    chat works

--- SocketManager.js
+++ SocketManager.js
@@ -8,9 +8,11 @@ class SocketManager {
   messageReceived(chatId){
     this.connectToNamespace(chatId);
     this.socket.emit("NEW_MESSAGE");
+    console.log('EMITTED MESSAGE');
   }
  
   connectToNamespace (nsp){
+    
     this.socket = this.io.of('/'+nsp);
     this.socket.on('connection', (sk) => {
       console.log('user connected')
